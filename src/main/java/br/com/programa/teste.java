cbb0c014bbb7232aad3c2409ae4d03146cf48a46
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-06-23 11:11:15 -0300

    Tirando imports desnecessarios

--- src/main/java/br/com/programa/teste.java
+++ src/main/java/br/com/programa/teste.java
@@ -3,8 +3,6 @@ package br.com.programa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.programa.model.enumerados.Cor;
-
 @SpringBootApplication
 public class teste {
 
